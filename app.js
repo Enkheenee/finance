7ddabf739b74afb5a552e01a32d27a0b24411d4e
Enkhbayar <enkheenee_m@yahoo.com>
2021-10-12 21:28:28 +0800

    finance module ruu item add hiideg bolgono.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var uiController = (function () {
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
-        discription: document.querySelector(DOMstrings.inputDescription).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value,
       };
     },
@@ -24,37 +24,66 @@ var uiController = (function () {
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {
+  // private function
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // private function
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
-      exp: []
-    }
+      exp: [],
+    },
 
     totals: {
       inc: 0,
-      exp: 0
-    }
-  }
+      exp: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 
 // Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
+
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
+
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
